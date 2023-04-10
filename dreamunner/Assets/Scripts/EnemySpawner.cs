1147f30e60c1022283bd5ccfe13d3b9846092dc9
Dmitry Lukashov <dmitrybanzi@gmail.com>
2023-04-10 20:45:29 +0600

    Refactoring EnemySpawner script
    * Upd capsule collider for Enemy

--- dreamunner/Assets/Scripts/EnemySpawner.cs
+++ dreamunner/Assets/Scripts/EnemySpawner.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] enemyVariants;
-    private float timeBtwSpawn;
-    public float startTimeBtwSpawn;
-    public float decreaseTime;
+    public float startTimeBtwSpawn = 2f;
+    public float decreaseTime = 0.1f;
     public float minTime = 0.65f;
+
+    private float timeBtwSpawn;
 
     private void Update()
     {
-        if(timeBtwSpawn <= 0)
+        if (timeBtwSpawn <= 0)
         {
             int rand = Random.Range(0, enemyVariants.Length);
             Instantiate(enemyVariants[rand], transform.position, Quaternion.identity);
             timeBtwSpawn = startTimeBtwSpawn;
-            if(startTimeBtwSpawn > minTime)
+            if (startTimeBtwSpawn > minTime)
             {
                 startTimeBtwSpawn -= decreaseTime;
             }
